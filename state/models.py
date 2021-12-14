77197e8f1f5da370389c0dc103960333dec75e10
morteza <mortezadavoudipr@gmail.com>
2021-12-14 12:51:51 +0330

    update urls

--- state/models.py
+++ state/models.py
@@ -7,4 +7,3 @@ class State(models.Model):
     status = models.BooleanField(default=True)
     title = models.CharField(max_length=50)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-
