597ad878554b8119e14c5fcecd2676705d142d8e
hamidreza <hamidrezamm9210@gmail.com>
2021-12-31 20:11:50 +0330

    api list projects

--- projects/apiurls.py
+++ projects/apiurls.py
@@ -3,6 +3,9 @@ from .api import *
 
 urlpatterns = [
     path('job_field_parent/', get_job_field_parent),
+    path('get_masters_list/', get_masters_list),
+    path('get_projects_list/', get_projects_list),
+    path('get_job_field/', get_job_field),
     path('job_field/<int:id>/', get_job_field),
     path('create_projects/', create_projects),
 ]
