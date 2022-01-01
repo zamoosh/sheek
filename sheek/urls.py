de970d3780d705e6a59d16eacd005f5b77055dbe
morteza <mortezadavoudipr@gmail.com>
2022-01-01 20:10:50 +0330

    chunk upload configurations and ..........

--- sheek/urls.py
+++ sheek/urls.py
@@ -49,3 +49,4 @@ urlpatterns = [
     path('jobs/', include('projects.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
