f6bb8b6d6537a2b31861fc8b7a9113714d243335
hamidreza <hamidrezamm9210@gmail.com>
2022-04-28 15:10:49 +0430

    cosher

--- projects/views/complaints.py
+++ projects/views/complaints.py
@@ -3,6 +3,7 @@ from .imports import *
 def complaints(request):
     context = {}
     context['ecomplaints'] = Complaint.objects.filter(project__in=Project.objects.filter(owner=request.user))
+    print('cossher')
     return render(request, 'project/complaints.html', context)
 
 
