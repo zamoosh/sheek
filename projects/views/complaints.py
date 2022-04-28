14f79878a0f3af81f5faf6ab1be9711c1a336cf6
mdavoodi <mortezadavodipr@gmail.com>
2022-04-28 14:55:19 +0430

    test confili

--- projects/views/complaints.py
+++ projects/views/complaints.py
@@ -2,7 +2,7 @@ from .imports import *
 
 def complaints(request):
     context = {}
-    context['complaintse'] = Complaint.objects.filter(project__in=Project.objects.filter(owner=request.user))
+    context['ecomplaints'] = Complaint.objects.filter(project__in=Project.objects.filter(owner=request.user))
     return render(request, 'project/complaints.html', context)
 
 
