5d4afbf12db1a96cb5ccb010a0fd9f27a2d4dc26
mho3en <hh.vaez@gmail.com>
2022-05-11 13:03:32 +0430

    fix bug message and update version.txt

--- library/message.py
+++ library/message.py
@@ -2,15 +2,18 @@ from django.db.models import Q
 
 from projects.models import *
 
+
 class SimpleMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        projects = Project.objects.filter(Q(owner=request.user) | Q(user_jobField__owner=request.user))
-        q = Q()
-        q = q & Q(Q(user_view=False) | Q(expert_view=False))
-        message = Message.objects.filter(q & ~Q(owner=request.user), project__in=projects)
-        request.message = message
-        response = self.get_response(request)
-        return response
+        if request.user.is_authenticated:
+            projects = Project.objects.filter(Q(owner=request.user) | Q(user_jobField__owner=request.user))
+            q = Q()
+            q = q & Q(Q(user_view=False) | Q(expert_view=False))
+            message = []
+            message = Message.objects.filter(q & ~Q(owner=request.user), project__in=projects)
+            request.message = message
+            response = self.get_response(request)
+            return response
