70bdd6adebe2b9e3b20ff0a73b4f52f82230a0b3
mortezadavoudi <mortezadavoudipr@gmail.com>
2022-07-05 16:15:06 +0430

    fix bug add salahiat

--- projects/views/set_tags.py
+++ projects/views/set_tags.py
@@ -7,5 +7,6 @@ def set_tags(request):
     q = Q(competence=True)
     if request.method == 'POST':
         context['searchbar'] = request.POST.get('searchbar')
-    context['salahiats'] = JobField.objects.filter(title__icontains=context['searchbar'], status=True)
+        context['salahiats'] = JobField.objects.filter(title__icontains=context['searchbar'], status=True)
+    context['salahiats'] = JobField.objects.filter(status=True)
     return render(request, 'project/set_tags.html', context)
